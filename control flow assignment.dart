532bffef19a787fafcbfcc89ee8e0bdd414148bb
kheira <kheira.Je29@gmail.com>
2024-03-21 00:25:13 -0700

    initial commit

---
+++ control flow assignment.dart
@@ -0,0 +1,10 @@
+void main() {
+  var number = 12;
+  if (number > 10) {
+    print("your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("your number is equal to 10");
+  }
+}
